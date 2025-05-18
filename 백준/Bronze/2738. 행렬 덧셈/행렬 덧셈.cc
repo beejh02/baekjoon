e11edbadd093a065fe1fbb3fd885d3aa627aee7f
이준혁 <91623756+beejh02@users.noreply.github.com>
2025-05-18 22:25:24 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int n, m;
+
+	cin >> n >> m;
+
+	int arr1[101][101];
+	int arr2[101][101];
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> arr1[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> arr2[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << arr1[i][j] + arr2[i][j] << " ";
+		}
+	}
+}
