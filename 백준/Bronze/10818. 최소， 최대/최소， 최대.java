439d6356de7e1c1190fea1b2bdc9e467a601dc17
이준혁 <91623756+beejh02@users.noreply.github.com>
2026-02-24 01:45:39 +0900

    [Bronze III] Title: 최소, 최대, Time: 1232 ms, Memory: 109404 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,22 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        int n = sc.nextInt();
-        int[] numbers = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < n; i++) {
-            numbers[i] = sc.nextInt();
+        int[] numlist = new int[n];
+
+        for(int i = 0; i<n; i++) {
+            numlist[i] = Integer.parseInt(st.nextToken());
         }
 
-        int min = Arrays.stream(numbers).min().getAsInt();
-        int max = Arrays.stream(numbers).max().getAsInt();
-
-        System.out.println(min);
-        System.out.println(max);
+        Arrays.sort(numlist);
+        System.out.print(numlist[0] + " " + numlist[n-1]);
 
     }
 }
