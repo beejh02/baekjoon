85260e1c09daeefa627a371543abe8c72cdb6f2c
이준혁 <91623756+beejh02@users.noreply.github.com>
2026-02-26 00:00:29 +0900

    [Bronze II] Title: 나머지, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<Integer> setArray = new HashSet<>();
+
+        for(int i = 0; i < 10; i++) {
+            int value = Integer.parseInt(br.readLine());
+            
+            setArray.add(value%42);
+        }
+
+        System.out.println(setArray.size());
+
+    }
+}
