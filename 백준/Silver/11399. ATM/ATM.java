eefc3c70aec7bcf11815313f67f7f355209d61be
이준혁 <91623756+beejh02@users.noreply.github.com>
2026-03-06 01:06:42 +0900

    [Silver IV] Title: ATM, Time: 116 ms, Memory: 14452 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int capacity = Integer.parseInt(br.readLine());
+        int[] arr = new int[capacity];
+        int[] result = new int[capacity];
+        int summary = 0;
+        int answer = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < capacity; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < capacity; i++) {
+            summary += arr[i];
+            result[i] = summary;
+        }
+
+        for (int item : result) {
+            answer += item;
+        }
+
+        System.out.println(answer);
+    }
+}
