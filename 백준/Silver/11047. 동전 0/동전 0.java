4d644c44083ab5d418ea88d2a5e4e06216204fea
이준혁 <91623756+beejh02@users.noreply.github.com>
2026-03-07 21:03:00 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,29 +1,33 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+class Main {
+    public static void main(String args[]) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        int play = Integer.parseInt(st.nextToken());
+        int goal = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
+        int[] arr = new int[play];
+        for (int i = 0; i < play; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
+        System.out.println(counting(play, goal, arr));
+    }
+
+    public static int counting(int capacity, int value, int[] array) {
         int count = 0;
-        for (int i = N - 1; i >= 0; i--) {
-            if (arr[i] <= K) {
-                count += K / arr[i];
-                K %= arr[i];
+        for (int i = capacity - 1; i >= 0; i--) {
+            if (array[i] <= value) {
+                count += (value / array[i]);
+                value %= array[i];
             }
         }
-
-        System.out.println(count);
+        return count;
     }
 }
