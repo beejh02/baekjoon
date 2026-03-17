157bc0c9da46637b012ff57a2c4cb8238b8cfc0c
이준혁 <91623756+beejh02@users.noreply.github.com>
2026-03-17 22:45:27 +0900

    [Silver I] Title: 안전 영역, Time: 1200 ms, Memory: 35160 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -0,0 +1,34 @@
+import sys
+sys.setrecursionlimit(100000)
+
+def DFS(rainfall, nowy, nowx):
+    if(visited[nowy][nowx] == False and arr[nowy][nowx] > rainfall):
+        visited[nowy][nowx] = True
+        for dy, dx in directions:
+            ny, nx = dy+nowy, dx+nowx
+            if(0 <= ny < n and 0 <= nx < n and arr[ny][nx] > rainfall and visited[ny][nx] == False):
+                DFS(rainfall, ny, nx)
+
+
+n = int(input())
+directions = [(-1,0), (0,1), (1,0), (0,-1)]
+arr = []
+ans = 0
+for i in range(n):
+    arr.append(list(map(int,input().split())))
+max_height = max(max(_) for _ in arr)
+
+
+for h in range(max_height + 1):
+    visited = [[False] * n for _ in range(n)]
+    count = 0
+    
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] > h and not visited[i][j]:
+                DFS(h, i, j)
+                count += 1
+
+    ans = max(ans, count)
+
+print(ans)
