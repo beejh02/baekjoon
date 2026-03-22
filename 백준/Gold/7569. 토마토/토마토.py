8bb1b33d0c2287da2adf51ceb4b3423d96ae0fe8
이준혁 <91623756+beejh02@users.noreply.github.com>
2026-03-22 14:49:53 +0900

    [Gold V] Title: 토마토, Time: 3440 ms, Memory: 50392 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -7,32 +7,40 @@ m, n, h = map(int, input().split())
 
 arr = []
 queue = deque([])
+left = 0
 
 for i in range(h):
-    layer = [list(map(int, input().split())) for j in range(n)]
+    layer = []
     for j in range(n):
+        row = list(map(int, input().split()))
         for k in range(m):
-            if layer[j][k] == 1:
+            if row[k] == 1:
                 queue.append((i, j, k))
+            elif row[k] == 0:
+                left += 1
+        layer.append(row)
     arr.append(layer)
+
+if left == 0:
+    print(0)
+    exit()
 
 directions = [(-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, 1), (0, 0, -1)]
 
-while queue:
-    curh, cury, curx = queue.popleft()
-    for dh, dy, dx in directions:
-        nh, ny, nx = dh + curh, dy + cury, dx + curx
-        if 0 <= nh < h and 0 <= ny < n and 0 <= nx < m and arr[nh][ny][nx] == 0:
-            arr[nh][ny][nx] = arr[curh][cury][curx] + 1
-            queue.append([nh, ny, nx])
-
 cnt = 0
-for i in range(h):
-    for j in range(n):
-        for k in range(m):
-            if arr[i][j][k] == 0:
-                print(-1)
-                exit()
-            cnt = max(cnt, arr[i][j][k])
+while queue:
+    for _ in range(len(queue)):
+        curh, cury, curx = queue.popleft()
+        for dh, dy, dx in directions:
+            nh, ny, nx = dh + curh, dy + cury, dx + curx
+            if 0 <= nh < h and 0 <= ny < n and 0 <= nx < m and arr[nh][ny][nx] == 0:
+                arr[nh][ny][nx] = 1
+                left -= 1
+                queue.append((nh, ny, nx))
+    if queue:
+        cnt += 1
 
-print(cnt - 1)
+if left > 0:
+    print(-1)
+else:
+    print(cnt)
