9fa5c50c0231158b8a19e16ceb98285574556733
이준혁 <91623756+beejh02@users.noreply.github.com>
2026-03-24 17:36:16 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 556 ms, Memory: 35668 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,19 +1,32 @@
 import sys
-from bisect import bisect_left
 
 input = sys.stdin.readline
 
 t = int(input())
 
-for i in range(t):
+for _ in range(t):
     n, m = map(int, input().split())
     a_group = list(map(int, input().split()))
     b_group = list(map(int, input().split()))
 
     b_group.sort()
     
-    count = 0
-    for a_item in a_group:
-        count += bisect_left(b_group, a_item)
+    total_count = 0
     
-    print(count)
+    for a in a_group:
+        left = 0
+        right = m - 1
+        current_count = 0
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if b_group[mid] < a:
+                current_count = mid + 1
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        total_count += current_count
+    
+    print(total_count)
