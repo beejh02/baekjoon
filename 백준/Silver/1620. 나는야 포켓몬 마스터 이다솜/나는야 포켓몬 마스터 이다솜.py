9a0ac0afeb38f379afccce8d9ec5023feea6a920
이준혁 <91623756+beejh02@users.noreply.github.com>
2026-03-30 16:34:38 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 244 ms, Memory: 54004 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,14 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
 n, m = map(int,input().split())
 
 dict = {}
-command = []
 for i in range(1, n+1):
-    dict.update({str(i) : input()}) 
+    dict.update({str(i) : input().rstrip()}) 
 
 reversed_dict = {v: k for k, v in dict.items()}
 
 for i in range(m):
-    command = input()
+    command = input().rstrip()
     if(command.isdigit()):
         print(dict[command])
     else:
