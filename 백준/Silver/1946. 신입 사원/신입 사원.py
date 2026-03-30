8a57ca4b1e07fa176987f1cde5650fced5aa5013
이준혁 <91623756+beejh02@users.noreply.github.com>
2026-03-30 10:45:15 +0900

    [Silver I] Title: 신입 사원, Time: 3324 ms, Memory: 60108 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,29 +1,21 @@
 import sys
 
-input_data = sys.stdin.read().split()
-ptr = 0
+input = sys.stdin.readline
 
-if input_data:
-    t = int(input_data[ptr])
-    ptr += 1
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    
+    grades = [tuple(map(int, input().split())) for _ in range(n)]
 
-    for _ in range(t):
-        n = int(input_data[ptr])
-        ptr += 1
+    grades.sort()
+    
+    cnt = 1
+    standard_min = grades[0][1]
 
-        grades = []
-        for _ in range(n):
-            grades.append([int(input_data[ptr]), int(input_data[ptr+1])])
-            ptr += 2
+    for i in range(1, n):
+        if grades[i][1] < standard_min:
+            cnt += 1
+            standard_min = grades[i][1]
 
-        grades.sort()
-        
-        cnt = 1
-        standard_min = grades[0][1]
-
-        for i in range(1, n):
-            if grades[i][1] < standard_min:
-                cnt += 1
-                standard_min = grades[i][1]
-
-        print(cnt)
+    print(cnt)
