0438e85f2b4a1864b57cbe14fe858e937aedf875
이준혁 <91623756+beejh02@users.noreply.github.com>
2026-04-01 21:26:05 +0900

    [Silver V] Title: 거스름돈, Time: 84 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.py
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -1,16 +1,17 @@
 n = int(input())
 
-cnt = 0
+dp = [-1] * (n+6)
+dp[2] = 1
+dp[4] = 2
+dp[5] = 1
 
-while(1):
-    if(n%5 == 0):
-        cnt += n//5
-        print(cnt)
-        break
-    else:
-        n -= 2
-        cnt += 1
-    
-    if(n < 0):
-        print(-1)
-        break
+for i in range(6, n+1):
+    if(dp[i-2] != -1):
+        dp[i] = dp[i-2] + 1
+
+
+    if(dp[i-5] != -1):
+        if(dp[i] == -1 or dp[i] > dp[i-5] + 1):
+            dp[i] = dp[i-5] + 1
+
+print(dp[n])
