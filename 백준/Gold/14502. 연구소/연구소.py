8190f5bb2df2701a3373ece892691e44ecd353e8
이준혁 <91623756+beejh02@users.noreply.github.com>
2026-04-02 23:09:55 +0900

    [Gold IV] Title: 연구소, Time: 1220 ms, Memory: 35028 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,24 +1,6 @@
 from collections import deque
 
-directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-
-n, m = map(int, input().split())
-original = []
-virus_pos = []
-empty_pos = []
-
-for i in range(n):
-    row = list(map(int, input().split()))
-    original.append(row)
-    for j in range(m):
-        if row[j] == 2:
-            virus_pos.append((i, j))
-        elif row[j] == 0:
-            empty_pos.append((i, j))
-
-queue = deque([])
-
-def bfs(map_copy, que):
+def bfs(temp_map, virus_pos, que, n, m, directions):
     que.clear()
     for pos in virus_pos:
         que.appendleft(pos)
@@ -27,32 +9,54 @@ def bfs(map_copy, que):
         y, x = que.pop()
         for dy, dx in directions:
             ny, nx = y + dy, x + dx
-            if 0 <= ny < n and 0 <= nx < m and map_copy[ny][nx] == 0:
-                map_copy[ny][nx] = 2
+            if 0 <= ny < n and 0 <= nx < m and temp_map[ny][nx] == 0:
+                temp_map[ny][nx] = 2
                 que.appendleft((ny, nx))
 
-ans = 0
-empty_cnt = len(empty_pos)
+def main():
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
-for i in range(empty_cnt):
-    for j in range(i + 1, empty_cnt):
-        for k in range(j + 1, empty_cnt):
-            temp_map = [row[:] for row in original]
-            
-            y1, x1 = empty_pos[i]
-            y2, x2 = empty_pos[j]
-            y3, x3 = empty_pos[k]
-            
-            temp_map[y1][x1] = 1
-            temp_map[y2][x2] = 1
-            temp_map[y3][x3] = 1
-            bfs(temp_map, queue)
-            
-            score = 0
-            for row in temp_map:
-                score += row.count(0)
-            
-            if score > ans:
-                ans = score
+    n, m = map(int, input().split())
+    original = []
+    virus_pos = []
+    empty_pos = []
 
-print(ans)
+    for i in range(n):
+        row = list(map(int, input().split()))
+        original.append(row)
+        for j in range(m):
+            if row[j] == 2:
+                virus_pos.append((i, j))
+            elif row[j] == 0:
+                empty_pos.append((i, j))
+
+    queue = deque([])
+    ans = 0
+    empty_cnt = len(empty_pos)
+
+    for i in range(empty_cnt):
+        for j in range(i + 1, empty_cnt):
+            for k in range(j + 1, empty_cnt):
+                temp_map = [row[:] for row in original]
+                
+                y1, x1 = empty_pos[i]
+                y2, x2 = empty_pos[j]
+                y3, x3 = empty_pos[k]
+                
+                temp_map[y1][x1] = 1
+                temp_map[y2][x2] = 1
+                temp_map[y3][x3] = 1
+                
+                bfs(temp_map, virus_pos, queue, n, m, directions)
+                
+                score = 0
+                for row in temp_map:
+                    score += row.count(0)
+                
+                if score > ans:
+                    ans = score
+
+    print(ans)
+
+if __name__ == "__main__":
+    main()
