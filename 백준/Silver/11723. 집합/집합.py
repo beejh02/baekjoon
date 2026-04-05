bc5d40e2d22485a6468fcfe1679f63c763a67967
이준혁 <91623756+beejh02@users.noreply.github.com>
2026-04-05 22:36:25 +0900

    [Silver V] Title: 집합, Time: 2476 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,24 +1,28 @@
 import sys
 input = sys.stdin.readline
 
-m = int(input())
-S = 0
+def main():
+    m = int(input())
+    S = 0
 
-for _ in range(m):
-    line = input().split()
-    cmd = line[0]
-    
-    if cmd == "all":
-        S = (1 << 21) - 1
-    elif cmd == "empty":
-        S = 0
-    else:
-        num = int(line[1])
-        if cmd == "add":
-            S |= (1 << num)
-        elif cmd == "remove":
-            S &= ~(1 << num)
-        elif cmd == "check":
-            print(1 if S & (1 << num) else 0)
-        elif cmd == "toggle":
-            S ^= (1 << num)
+    for _ in range(m):
+        line = input().split()
+        cmd = line[0]
+        
+        if cmd == "all":
+            S = (1 << 21) - 1
+        elif cmd == "empty":
+            S = 0
+        else:
+            num = int(line[1])
+            if cmd == "add":
+                S |= (1 << num)
+            elif cmd == "remove":
+                S &= ~(1 << num)
+            elif cmd == "check":
+                print(1 if S & (1 << num) else 0)
+            elif cmd == "toggle":
+                S ^= (1 << num)
+
+if __name__ == "__main__":
+    main()
